f018ba5207f23e846054c8c01de9c6c969f4db12
Liisagrant <lisagrant1990@gmail.com>
2022-02-15 12:45:39 +0100

    added

--- 5-Filter-Method-example/js/script.js
+++ 5-Filter-Method-example/js/script.js
@@ -1,3 +1,9 @@
 // Filter method Example
 
 // Example 1 Filter Method with Static Data
+
+import teams from "./data/teams.js";
+
+console.log(teams);
+
+const teamContainer = document.query
---
+++ string-manipulation/js/script.js
@@ -0,0 +1,3 @@
+const name = "Lisa";
+const nameLength = name.length;
+console.log(nameLength);
