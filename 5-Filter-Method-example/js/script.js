e121b0517f7cee1dcdeec2fe06523e97d2fb0642
Liisagrant <lisagrant1990@gmail.com>
2022-02-15 14:54:42 +0100

    using starWith and keyup.

--- 5-Filter-Method-example/js/script.js
+++ 5-Filter-Method-example/js/script.js
@@ -1,9 +1,44 @@
 // Filter method Example
 
 // Example 1 Filter Method with Static Data
-
-import teams from "./data/teams.js";
-
+import teams from "./data/teams.js"
 console.log(teams);
 
-const teamContainer = document.query
+//2. select the teams-container class
+//3. select the search class
+
+const teamContainer = document.querySelector(".team-container");
+const search = document.querySelector(".search");
+
+//4. assign the teams data to a variable
+
+let teamsToRender = teams;
+
+//5. loop on the array and inject data to the DOM teamContainer
+function renderTeams (someArr){
+    teamContainer.innerHTML = "";
+    for(let j = 0; j < someArr.length; j++){
+        console.log(someArr[j]);
+        teamContainer.innerHTML += `<div class="team"><h3>${someArr[j].full_name}</h3></div>`
+    }
+}
+renderTeams(teamsToRender);
+
+search.onkeyup = function (event) {
+    console.log(event);
+    //console.log(event.target.value);
+    const searchValue = event.target.value.trim().toLowerCase();
+    console.log(`searchValue`, searchValue);
+
+    const filteredTeams = teamsToRender.filter(function (team){
+        if (team.full_name.toLowerCase().startsWith(searchValue)){
+            return true
+        }
+    });
+    console.log(filteredTeams);
+    //teamsToRender = filteredTeams;
+    console.log(`teamsToRender`,filteredTeams);
+    renderTeams(filteredTeams);
+}
+
+
